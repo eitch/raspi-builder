b34897c0db98f157aa3d3c23e27c8c9c9543f573
Robert von Burg <eitch@eitchnet.ch>
2020-03-04 15:37:16 +0100

    [Project] Renamed scripts to -64, instead of -RPi4

--- install-kernel-rpi4.sh
+++ install-kernel-rpi4.sh
@@ -34,7 +34,7 @@ if [[ -f "${toolTest}" ]] ; then
   echo "INFO: Toolchain already available at ${toolTest}"
 else
   echo "INFO: Building toolchain at ${TOOLCHAIN}"
-  ${SOURCEDIR}/build-toolchain-rpi4.sh
+  ${SOURCEDIR}/build-toolchain-64.sh
 fi
 
 kernelTest="${TOOLCHAIN}/rpi-linux/kernel-build/arch/arm64/boot/Image"
@@ -42,9 +42,9 @@ if [[ -f "${kernelTest}" ]] ; then
   echo "INFO: Kernel already available at ${kernelTest}"
 else
   echo "INFO: Building Kernel at ${TOOLCHAIN}"
-  ${SOURCEDIR}/build-kernel-rpi4.sh
+  ${SOURCEDIR}/build-kernel-64.sh
 fi
 
-"${SOURCEDIR}/copy-kernel-rpi4.sh" "${ROOTDIR}"
+"${SOURCEDIR}/copy-kernel-64.sh" "${ROOTDIR}"
 
 exit 0
